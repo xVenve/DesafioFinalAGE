39296c969a35d2d7638ac7e20d91f1ee5ae38854
Jorge Rodríguez <jrf1616@gmail.com>
2021-11-20 13:29:06 +0100

    Añadida escritura y lectura CSV en AgentCSV

--- src/test/java/SkeletonMain.java
+++ src/test/java/SkeletonMain.java
@@ -4,32 +4,34 @@ import java.util.Collections;
 
 public class SkeletonMain {
 
-    public static void main(String[] args) {
-        // Uncomment this section and comment the other one to create a Solo Game
-        /* Solo Game */
-        SoloGameRunner gameRunner = new SoloGameRunner();
-        // Sets the player
-        gameRunner.setAgent(Agent1.class);
-        // gameRunner.setAgent(Agent2.class);
+  public static void main(String[] args) {
+    // Uncomment this section and comment the other one to create a Solo Game
+    /* Solo Game */
+    SoloGameRunner gameRunner = new SoloGameRunner();
+    // Sets the player
+    gameRunner.setAgent(AgentCSV.class);
+    // gameRunner.setAgent(Agent2.class);
 
-        // Sets a test case
-        gameRunner.setTestCase("test0.json");
+    // Sets a test case
+    gameRunner.setTestCase("test0.json");
 
-        // Another way to add a player for python
-        // gameRunner.addAgent("python3 /home/user/player.py");
+    // Another way to add a player for python
+    // gameRunner.addAgent("python3 /home/user/player.py");
 
-        // Start the game server
-        // gameRunner.start();
-        // Simulate
-        GameResult gameRunnerResult = new GameResult();
-        gameRunnerResult = gameRunner.simulate();
+    // Start the game server
+    // gameRunner.start();
+    // Simulate
+    GameResult gameRunnerResult = new GameResult();
+    gameRunnerResult = gameRunner.simulate();
 
-        float fitness = Float.parseFloat(
-                gameRunnerResult.metadata.split(":")[1].substring(1, gameRunnerResult.metadata.split(":")[1].length() - 3)
-        );
-
-        int numCheckpointCollected =
-                gameRunnerResult.summaries.size() -
-                        Collections.frequency(gameRunnerResult.summaries, "");
-    }
+    // Basic Fitness function, game score
+    float fitness = Float.parseFloat(
+      gameRunnerResult.metadata.split(":")[1].substring(1, gameRunnerResult.metadata.split(":")[1].length() - 3)
+    );
+    
+    // Number of Checkpoints collected
+    int numCheckpointCollected =
+      gameRunnerResult.summaries.size() -
+      Collections.frequency(gameRunnerResult.summaries, "");
+  }
 }
